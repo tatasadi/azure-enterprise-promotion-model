9c49bc835b5cc45998d1ad504f10455b524cc66a
Ehsan Tatasadi <ehsan@tatasadi.com>
2026-02-23 14:43:18 +0100

    update terraform storage to common

--- infrastructure/bootstrap/variables.tf
+++ infrastructure/bootstrap/variables.tf
@@ -13,7 +13,7 @@ variable "location" {
 variable "storage_account_name" {
   description = "Name of the storage account (must be globally unique)"
   type        = string
-  default     = "sttfstatedev001"
+  default     = "sttfstatecommon001"
 }
 
 variable "replication_type" {
